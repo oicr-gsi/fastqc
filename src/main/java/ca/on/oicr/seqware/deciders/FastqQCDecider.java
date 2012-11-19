7825161c7f37021e69efe5811337119b565b3b54
Morgan Taschuk <mtaschuk@sqwdev.res.oicr.on.ca>
2012-11-19 10:45:29 -0500

    Added in the output-path and output-folder parameters
    Former-commit-id: 231110953d36a3a6a84576b188d757d55297c6d2

--- src/main/java/ca/on/oicr/seqware/deciders/FastqQCDecider.java
+++ src/main/java/ca/on/oicr/seqware/deciders/FastqQCDecider.java
@@ -7,6 +7,7 @@ import net.sourceforge.seqware.common.module.ReturnValue;
 import net.sourceforge.seqware.common.util.Log;
 import net.sourceforge.seqware.common.util.maptools.MapTools;
 import net.sourceforge.seqware.pipeline.deciders.BasicDecider;
+import java.io.File;
 
 /**
  * @author zhibin.lu@oicr.on.ca
@@ -15,11 +16,17 @@ import net.sourceforge.seqware.pipeline.deciders.BasicDecider;
 public class FastqQCDecider extends BasicDecider {
 
     private Map<String, String> pathToType = new HashMap<String, String>();
-
+    private String folder = "seqware-results";
+    private String path = "./";
+    private String iniFile=null;
     public FastqQCDecider() {
         super();
         parser.acceptsAll(Arrays.asList("ini-file"), "Optional: the location of the INI file.").withRequiredArg();
         parser.accepts("extract"); //whether to extract the final QC zip file
+        parser.accepts("output-folder", "Optional: the name of the folder to put the output into relative to the output-path. "
+                + "Corresponds to output-dir in INI file. Default: seqware-results").withRequiredArg();
+        parser.accepts("output-path", "Optional: the path where the files should be copied to "
+                + "after analysis. Corresponds to output-prefix in INI file. Default: ./").withRequiredArg();
 
     }
 
@@ -34,6 +41,30 @@ public class FastqQCDecider extends BasicDecider {
         if (this.options.has("group-by")) {
             Log.error("I think your workflow run will fail. This fastq-qc workflow handles each fastq file at a time. Please do not use 'group-by' option.");
         }
+        if (options.has("ini-file")) {
+            File file = new File(options.valueOf("ini-file").toString());
+            if (file.exists()) {
+                iniFile = file.getAbsolutePath();
+                Map<String, String> iniFileMap = MapTools.iniString2Map(iniFile);
+                folder = iniFileMap.get("output_dir");
+                path = iniFileMap.get("output_path");
+            } else {
+                Log.error("The given INI file does not exist: " + file.getAbsolutePath());
+                System.exit(1);
+            }
+
+        }
+        if (options.has("output-folder")) {
+            folder = options.valueOf("output-folder").toString();
+        }
+        if (options.has("output-path")) {
+            path = options.valueOf("output-path").toString();
+            if (!path.endsWith("/")) {
+                path += "/";
+            }
+        }
+
+
         return val;
 
     }
@@ -56,7 +87,7 @@ public class FastqQCDecider extends BasicDecider {
         //Load the user-defined ini-file
         Map<String, String> iniFileMap = new TreeMap<String, String>();
         if (options.has("ini-file")) {
-            MapTools.ini2Map((String) options.valueOf("ini-file"), iniFileMap, false);
+            MapTools.ini2Map(iniFile, iniFileMap, false);
         }
 
         //if the command line has 'extract' option, the final zip file will also be extracted.
@@ -66,6 +97,8 @@ public class FastqQCDecider extends BasicDecider {
         //FastQC workflow only handles one file at a time, so the commaSeparatedFilePaths should be a single file name. Warning has been given
         //at when the class instance was created. It is users' responsibility not to use 'group-by' option.
         iniFileMap.put("input_file", commaSeparatedFilePaths);
+        if (folder!=null) iniFileMap.put("output_dir", folder);
+        if (path!=null) iniFileMap.put("output_prefix", path);
 
         return iniFileMap;
     }
