b6a01b0fdc8c08a6c6b86ead8d3f19c2241b2332
Michael Laszloffy <michael.laszloffy@oicr.on.ca>
2013-08-12 16:49:11 -0400

    FastQC decider processing of files by IUS swid rather than file swid; FastQC workflow now handles multiple input files
    PDE-224
    PDE-225
    SEQPRODBIO-1838
    
    
    Former-commit-id: 22bbc972eff9ae65ef3ea097316c161177793ca6

--- java/src/main/java/ca/on/oicr/pde/workflows/WorkflowClient.java
+++ java/src/main/java/ca/on/oicr/pde/workflows/WorkflowClient.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import net.sourceforge.seqware.common.util.Log;
 import net.sourceforge.seqware.pipeline.workflowV2.AbstractWorkflowDataModel;
 import net.sourceforge.seqware.pipeline.workflowV2.model.Command;
 import net.sourceforge.seqware.pipeline.workflowV2.model.Job;
@@ -16,7 +17,7 @@ public class WorkflowClient extends AbstractWorkflowDataModel {
     //private String extract = null;
     //private boolean doExtract = false;
     private String queue = null;
-    private String inputFile = null;
+    private String inputFiles = null;
     private String outputPrefix = null;
     private String outputDir = null;
     private String outputPath = null;
@@ -40,7 +41,7 @@ public class WorkflowClient extends AbstractWorkflowDataModel {
             outputDir = getProperty("output_dir");
             outputPrefix = getProperty("output_prefix");
             outputPath = getProperty("output_path");
-            
+
             if (Arrays.asList("na", "").contains(outputPath.toLowerCase().trim())) {
                 finalOutputDir = outputPrefix + outputDir + "/seqware-" + getSeqware_version() + "_" + getName() + "_" + getVersion() + "/" + getRandom() + "/";
             } else {
@@ -48,14 +49,14 @@ public class WorkflowClient extends AbstractWorkflowDataModel {
                 outputPath = outputPath.lastIndexOf("/") == (outputPath.length() - 1) ? outputPath : outputPath + "/";
                 finalOutputDir = outputPath;
             }
-            
+
             perl = binDir + getProperty("perl");
             java = binDir + getProperty("java");
             fastqc = binDir + getProperty("fastqc");
             //extract = getProperty("extract");        
             //doExtract = extract.equals("yes") ? true : false;
             queue = getProperty("queue");
-            inputFile = getProperty("input_file");
+            inputFiles = getProperty("input_files");
         } catch (Exception ex) {
             logger.log(Level.SEVERE, "Expected parameter missing", ex);
         }
@@ -73,12 +74,14 @@ public class WorkflowClient extends AbstractWorkflowDataModel {
 
     @Override
     public Map<String, SqwFile> setupFiles() {
-
-        SqwFile file0 = this.createFile("file_in_0");
-        file0.setSourcePath(inputFile);
-        file0.setType("chemical/seq-na-fastq");
-        file0.setIsInput(true);
-
+        
+        int fileNumber = 0;
+        for (String inputFilePath : inputFiles.split(",")) {
+            SqwFile file = this.createFile("file_in_" + fileNumber++);
+            file.setSourcePath(inputFilePath);
+            file.setType("chemical/seq-na-fastq");
+            file.setIsInput(true);
+        }
         return this.getFiles();
 
     }
@@ -86,19 +89,24 @@ public class WorkflowClient extends AbstractWorkflowDataModel {
     @Override
     public void buildWorkflow() {
 
-        Job job00 = getFastQcJob();
-        job00.setMaxMemory("4000");
-        job00.setQueue(queue);
+        //Launch a fastq qc job for each input file
+        for (Map.Entry<String, SqwFile> file : this.getFiles().entrySet()) {
+            logger.info(String.format("%s %s", file.getKey(), file.getValue().getProvisionedPath()));
+            Job job = getFastQcJob(file.getValue().getProvisionedPath());
+            job.setMaxMemory("4000");
+            job.setQueue(queue);
+        }
 
     }
 
-    private Job getFastQcJob() {
+    private Job getFastQcJob(String fastqInputFilePath) {
 
         Job job = getWorkflow().createBashJob("FastQC");
         Command command = job.getCommand();
         command.addArgument(perl);
         command.addArgument(fastqc);
-        command.addArgument(getFiles().get("file_in_0").getProvisionedPath());
+        //command.addArgument(getFiles().get("file_in_0").getProvisionedPath());
+        command.addArgument(fastqInputFilePath);
         command.addArgument("--java=" + java);
 
 //        //extraction of fastqc report zip disabled
@@ -108,8 +116,8 @@ public class WorkflowClient extends AbstractWorkflowDataModel {
 
         command.addArgument("--outdir " + dataDir);
 
-        String outputFile = inputFile.substring(inputFile.lastIndexOf("/") + 1, inputFile.lastIndexOf(".fastq")) + "_fastqc.zip";
-        SqwFile sqwOutputFile = createOutFile(dataDir + outputFile, "application/zip-report-bundle", finalOutputDir + outputFile, true);
+        String outputFileName = fastqInputFilePath.substring(fastqInputFilePath.lastIndexOf("/") + 1, fastqInputFilePath.lastIndexOf(".fastq")) + "_fastqc.zip";
+        SqwFile sqwOutputFile = createOutFile(dataDir + outputFileName, "application/zip-report-bundle", finalOutputDir + outputFileName, true);
         job.addFile(sqwOutputFile);
 
 //        //extraction of fastqc report zip disabled
