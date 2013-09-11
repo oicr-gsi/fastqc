859b3b656f912fc833b33eb24bd12de3f04c61fb
Michael Laszloffy <michael.laszloffy@oicr.on.ca>
2013-09-11 16:30:47 -0400

    Implementation of a new grouping strategy for FastqQC Decider (Workflow run id then IUS id).
    PDE-301
    
    
    Former-commit-id: 66505d475f5591e290251f188b1c7e712c1efd68

--- src/main/java/ca/on/oicr/pde/deciders/FastqQCDecider.java
+++ src/main/java/ca/on/oicr/pde/deciders/FastqQCDecider.java
@@ -1,12 +1,12 @@
 package ca.on.oicr.pde.deciders;
 
 import java.util.*;
-import net.sourceforge.seqware.common.hibernate.FindAllTheFiles.Header;
 import net.sourceforge.seqware.common.module.FileMetadata;
 import net.sourceforge.seqware.common.module.ReturnValue;
 import net.sourceforge.seqware.common.util.Log;
 import net.sourceforge.seqware.common.util.maptools.MapTools;
 import java.io.File;
+import net.sourceforge.seqware.common.hibernate.FindAllTheFiles.Header;
 
 /**
  * @author zhibin.lu@oicr.on.ca
@@ -18,6 +18,10 @@ public class FastqQCDecider extends OicrDecider {
 //    private String folder = "seqware-results";
 //    private String path = "./";
     private String iniFile = null;
+    
+    //NOTE: order of headers in list matters (group by first, then second, etc)
+    private List<Header> orderedListOfHeadersToGroupBy = Arrays.asList(Header.WORKFLOW_RUN_SWA, Header.IUS_SWA);
+
     public FastqQCDecider() {
         super();
         parser.acceptsAll(Arrays.asList("ini-file"), "Optional: the location of the INI file.").withRequiredArg();
@@ -31,11 +35,11 @@ public class FastqQCDecider extends OicrDecider {
 
     @Override
     public ReturnValue init() {
-        this.setGroupingStrategy(Header.IUS_SWA);
+        
         this.setMetaType(Arrays.asList("chemical/seq-na-fastq", "chemical/seq-na-fastq-gzip"));
 
         ret = super.init();
-        
+
         //allows anything defined on the command line to override the 'defaults' here.
 
         if (this.options.has("group-by")) {
@@ -71,7 +75,49 @@ public class FastqQCDecider extends OicrDecider {
     }
 
     @Override
+    public Map<String, List<ReturnValue>> separateFiles(List<ReturnValue> vals, String groupBy) {
+
+        Map<String, List<ReturnValue>> map = new HashMap<String, List<ReturnValue>>();
+
+        //group files according to the designated header (e.g. sample SWID)
+        for (ReturnValue r : vals) {
+
+            StringBuilder keyBuilder = new StringBuilder();
+
+            //iterate through ordered list of headers to group by
+            for (Header h : orderedListOfHeadersToGroupBy) {
+
+                String subKey = r.getAttributes().get(h.getTitle());
+
+                if (subKey != null) {
+                    subKey = handleGroupByAttribute(subKey);
+                }
+
+                keyBuilder.append(String.format("[%s=%s] ", h.getTitle(), subKey));
+            }
+
+            String key = keyBuilder.toString();
+
+            List<ReturnValue> vs = map.get(key);
+            if (vs == null) {
+                vs = new ArrayList<ReturnValue>();
+            }
+            vs.add(r);
+            map.put(key, vs);
+        }
+
+        return map;
+    }
+
+    @Override
+    protected String handleGroupByAttribute(String attribute) {
+
+        return super.handleGroupByAttribute(attribute);
+    }
+
+    @Override
     protected boolean checkFileDetails(ReturnValue returnValue, FileMetadata fm) {
+
         pathToType.put(fm.getFilePath(), fm.getMetaType());
         //FastQC workflow only handles gzipped/unzipped FASTQ format.
         if (!fm.getMetaType().equals("chemical/seq-na-fastq") && !fm.getMetaType().equals("chemical/seq-na-fastq-gzip")) {
@@ -86,7 +132,7 @@ public class FastqQCDecider extends OicrDecider {
     protected Map<String, String> modifyIniFile(String commaSeparatedFilePaths, String commaSeparatedParentAccessions) {
 
         Map<String, String> iniFileMap = super.modifyIniFile(commaSeparatedFilePaths, commaSeparatedParentAccessions);
-        
+
         //Load the user-defined ini-file
         if (options.has("ini-file")) {
             MapTools.ini2Map(iniFile, iniFileMap, false);
@@ -95,7 +141,7 @@ public class FastqQCDecider extends OicrDecider {
 //        //Remove "input_files" from ini file - FastQC workflow only one input at a time.
 //        //"input_files" is added to the iniFileMap by BasicDecider (parent of OicrDecider).
 //        iniFileMap.remove("input_files");
-        
+
         //if the command line has 'extract' option, the final zip file will also be extracted.
 //        if (options.has("extract")) {
 //            iniFileMap.put("extract", "yes");
@@ -103,13 +149,12 @@ public class FastqQCDecider extends OicrDecider {
 //        //FastQC workflow only handles one file at a time, so the commaSeparatedFilePaths should be a single file name. Warning has been given
 //        //at when the class instance was created. It is users' responsibility not to use 'group-by' option.
 //        iniFileMap.put("input_file", commaSeparatedFilePaths);
-        
+
         iniFileMap.put("input_files", commaSeparatedFilePaths);
-        
+
 //        if (folder!=null) iniFileMap.put("output_dir", folder);
 //        if (path!=null) iniFileMap.put("output_prefix", path);
 
         return iniFileMap;
     }
-
 }
