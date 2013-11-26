3098190be94c4256500a9b6e3803d7e1d348a262
Roshaan Tahir <rtahir@platinum.oicr.on.ca>
2013-11-26 16:45:06 -0500

    fastqc updated regr. testing
    Former-commit-id: c366548c9954a0965c81bc0119e74ebc66708a47

--- java/src/test/resources/calculate.sh
+++ java/src/test/resources/calculate.sh
@@ -2,6 +2,7 @@
 cd $1
  
 find . -name "*.zip" -exec unzip {} \; >/dev/null
+find . -type f -name fastqc_report.html -exec sed -i '/<div id="header_filename">/,/<\/div>/{/<div id="header_filename">/!{/<\/div>/!d}}' {} \;
 find . -type f -not -path "./*.zip" -exec md5sum {} + 
  
 
