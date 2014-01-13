21622717ebd01a476b8f1a7db1f97d65b8ab7c12
Michael Laszloffy <michael.laszloffy@oicr.on.ca>
2014-01-13 16:22:07 -0500

    FastQC workflow formatting and comment cleanup
    Former-commit-id: 8c394d9f3cc3139a89bfe51d1b865a1388418110

--- src/main/java/ca/on/oicr/pde/workflows/WorkflowClient.java
+++ src/main/java/ca/on/oicr/pde/workflows/WorkflowClient.java
@@ -1,9 +1,7 @@
 package ca.on.oicr.pde.workflows;
 
 import ca.on.oicr.pde.utilities.workflows.OicrWorkflow;
-import java.util.Arrays;
 import java.util.Map;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import net.sourceforge.seqware.pipeline.workflowV2.model.Command;
 import net.sourceforge.seqware.pipeline.workflowV2.model.Job;
@@ -12,56 +10,32 @@ import net.sourceforge.seqware.pipeline.workflowV2.model.SqwFile;
 public class WorkflowClient extends OicrWorkflow {
 
     private static final Logger logger = Logger.getLogger(WorkflowClient.class.getName());
+
     //workflow parameters
-    //private String /extract = null;
-    //private boolean doExtract = false;
     private String queue = null;
     private String inputFiles = null;
-    //private String outputPrefix = null;
-    //private String outputDir = null;
-   // private String outputPath = null;
+    private Boolean manualOutput = false;
+
     //workflow programs
     private String perl = null;
     private String java = null;
     private String fastqc = null;
+
     //workflow directories
     private String binDir = null;
     private String dataDir = null;
-    //private String finalOutputDir = null;
-    private Boolean manualOutput = false;
-    
 
     //Constructor - called in setupDirectory()
     private void WorkflowClient() {
 
         binDir = getWorkflowBaseDir() + "/bin/";
         dataDir = "data/";
-
-//        //load workflow parameters
-        try {
-//            outputDir = getProperty("output_dir");
-//            outputPrefix = getProperty("output_prefix");
-//            outputPath = getProperty("output_path");
-//
-//            if (Arrays.asList("na", "").contains(outputPath.toLowerCase().trim())) {
-//                outputPath = outputPrefix + outputDir + "/seqware-" + getSeqware_version() + "_" + getName() + "_" + getVersion() + "/" + getRandom() + "/";
-//            } else {
-//                //make sure the path ends with a "/"
-//                outputPath = outputPath.lastIndexOf("/") == (outputPath.length() - 1) ? outputPath : outputPath + "/";
-////                finalOutputDir = outputPath;
-////            }
-            
-            manualOutput = Boolean.valueOf(getProperty("manual_output"));
-            perl = binDir + getProperty("perl");
-            java = binDir + getProperty("java");
-            fastqc = binDir + getProperty("fastqc");
-            //extract = getProperty("extract");        
-            //doExtract = extract.equals("yes") ? true : false;
-            queue = getOptionalProperty("queue", "");
-            inputFiles = getProperty("input_files");
-        } catch (Exception ex) {
-            logger.log(Level.SEVERE, "Expected parameter missing", ex);
-        }
+        manualOutput = Boolean.valueOf(getProperty("manual_output"));
+        perl = binDir + getProperty("perl");
+        java = binDir + getProperty("java");
+        fastqc = binDir + getProperty("fastqc");
+        queue = getOptionalProperty("queue", "");
+        inputFiles = getProperty("input_files");
 
     }
 
@@ -76,7 +50,7 @@ public class WorkflowClient extends OicrWorkflow {
 
     @Override
     public Map<String, SqwFile> setupFiles() {
-        
+
         int fileNumber = 0;
         for (String inputFilePath : inputFiles.split(",")) {
             SqwFile file = this.createFile("file_in_" + fileNumber++);
@@ -107,45 +81,17 @@ public class WorkflowClient extends OicrWorkflow {
         Command command = job.getCommand();
         command.addArgument(perl);
         command.addArgument(fastqc);
-        //command.addArgument(getFiles().get("file_in_0").getProvisionedPath());
         command.addArgument(fastqInputFilePath);
         command.addArgument("--java=" + java);
-
-//        //extraction of fastqc report zip disabled
-//        if (!doExtract) {
         command.addArgument("--noextract");
-//        }
-
         command.addArgument("--outdir " + dataDir);
 
         String outputFileName = fastqInputFilePath.substring(fastqInputFilePath.lastIndexOf("/") + 1, fastqInputFilePath.lastIndexOf(".fastq")) + "_fastqc.zip";
-        //SqwFile sqwOutputFile = createOutFile(dataDir + outputFileName, "application/zip-report-bundle", finalOutputDir + outputFileName, true);
         SqwFile sqwOutputFile = createOutputFile(dataDir + outputFileName, "application/zip-report-bundle", manualOutput);
         job.addFile(sqwOutputFile);
-
-//        //extraction of fastqc report zip disabled
-//        if(doExtract){
-//            String extractedOutputDir = outputFile.substring(0, outputFile.lastIndexOf(".zip")) + "/";
-//            SqwFile sqwExtractedOutputDir = createOutFile(dataDir + extractedOutputDir, "", outputDir + extractedOutputDir + dataDir, true);  //broken - does not provision directory recursively
-//            job.addFile(sqwExtractedOutputDir);
-//        }
 
         return job;
 
     }
 
-//    private SqwFile createOutFile(String sourcePath, String sourceType, String outputPath, boolean forceCopy) {
-//
-//        SqwFile file = createOutputFile(sourcePath, sourceType, manualOutput);
-////        file.setSourcePath(sourcePath);
-////        file.setType(sourceType);
-////        file.setIsOutput(true);
-////        file.setOutputPath(outputPath);
-////        file.setForceCopy(forceCopy);
-////
-////        return file;
-//
-//    }
-    
-    
 }
