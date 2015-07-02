c3d328dcc4a654c680c7dd33992591ad055cfc6f
Morgan Taschuk <morgan.taschuk@oicr.on.ca>
2015-07-02 17:33:46 -0400

    Restructured pde-dev directories to have subdirs named after tools; updated POM to match

--- src/test/resources/calculate.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-set -o nounset
-set -o errexit
-set -o pipefail
-set -o noclobber
-
-cd "$1"
-
-find . -type f -name "*fastqc.html" -printf '%p\t' -exec bash -c 'cat "$0" | sed "s/\(<div id=\"header_filename\">\)[^<]*\(<br\/>\)/\1\2/g" | md5sum | cut -d " " -f 1' {} \;
